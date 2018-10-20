4d767848ba5f14a503fa46c1b458776df999b465
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2018-10-20 18:01:51 +0200

    add separate settings file for sensitive data

---
+++ main/secret_settings.py
@@ -0,0 +1,1 @@
+SECRET_KEY = 'vb*r!n^#)6)a!n#4n8k4-ig!n)p_&#lsh-ltvqg#a_!8*319s5'
--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from .secret_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vb*r!n^#)6)a!n#4n8k4-ig!n)p_&#lsh-ltvqg#a_!8*319s5'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
